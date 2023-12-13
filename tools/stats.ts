e2ae098b2349d4876478267ae46453cc7eeba70d
cristian silva <cristiansilva@cristians-MacBook-Pro.local>
2023-12-12 20:49:50 -0700

    stats

--- tools/stats.ts
+++
@@ -1,9 +0,0 @@
-import { getQuestCorruption } from "./corruption";
-import { getDropRates } from "./quest";
-
-export async function updateStats() {
-    const corruptionData = getQuestCorruption();
-    const dropRatesData = getDropRates();
-    const [corruption, dropRates] = await Promise.all([corruptionData, dropRatesData])
-    console.log(corruption, dropRates)
-}
